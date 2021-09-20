5ed7298d77dc1b05e9e9edda928f160c43bca706
Ryan Hemmila <ryanhemmila@gmail.com>
2021-09-20 02:00:14 -0500

    changes

--- minilab3/systolic_array.sv
+++ minilab3/systolic_array.sv
@@ -38,20 +38,25 @@ module systolic_array
 
   assign Cout = cout_grid[Crow];
 
-  for (int i = 0; i < DIM; i = i + 1) begin
-    assign ain_grid[i][0] = A[i];
-    assign bin_grid[0][i] = B[i];
-  end
-  for (int i = 1; i < DIM; i = i + 1) begin
-    for (int j = 0; j < DIM; j = j + 1) begin
-      assign ain_grid[j][i] = aout_grid[j][i - 1];
-      assign bin_grid[i][j] = bout_grid[i - 1][j];
+  generate
+    genvar i, j;
+    genvar row, col;
+
+    for (i = 0; i < DIM; i = i + 1) begin : init
+      assign ain_grid[i][0] = A[i];
+      assign bin_grid[0][i] = B[i];
     end
-  end
-  for (int row = 0; row < DIM; row = row + 1) begin
-    for (int col = 0; col < DIM; col = col + 1) begin
-      assign cin_grid[row][col] = Cin[col];
-      assign wren_grid[row][col] = (Crow == row) ? WrEn : 1'b0;
+    for (i = 1; i < DIM; i = i + 1) begin : conn_i
+      for (j = 0; j < DIM; j = j + 1) begin : conn_j
+        assign ain_grid[j][i] = aout_grid[j][i - 1];
+        assign bin_grid[i][j] = bout_grid[i - 1][j];
+      end
     end
-  end
+    for (row = 0; row < DIM; row = row + 1) begin : wren_row
+      for (col = 0; col < DIM; col = col + 1) begin : wren_col
+        assign cin_grid[row][col] = Cin[col];
+        assign wren_grid[row][col] = (Crow == row) ? WrEn : 1'b0;
+      end
+    end
+  endgenerate
 endmodule
